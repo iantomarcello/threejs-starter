71fc648c24719a08eb2135a0ff47f9c04e5dbfe0
iantomarcello <ianyys94@gmail.com>
2021-02-01 10:39:31 +0800

    Refactor: Using * as import instead

--- worker.js
+++ worker.js
@@ -1,10 +1,10 @@
 import * as Comlink from 'comlink';
-import { Scene, PerspectiveCamera, WebGLRenderer, GridHelper, Mesh, BoxGeometry, MeshBasicMaterial } from 'three';
+import * as THREE from 'three';
 
 class Three {
   constructor() {
     this.canvas;
-    this.scene = new Scene();
+    this.scene = new THREE.Scene();
     this.camera;
     this.renderer;
     this.gridHelper;
@@ -17,19 +17,19 @@ class Three {
     let height = data.height;
     let pixelRatio = data.pixelRatio;
 
-    this.camera = new PerspectiveCamera(45, width / height, 0.1, 1000);
+    this.camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
     this.camera.position.y = 30;
 
-    this.renderer = new WebGLRenderer({ antialias: true, canvas: this.canvas});
+    this.renderer = new THREE.WebGLRenderer({ antialias: true, canvas: this.canvas});
     this.renderer.setSize(width, height, false);
     this.renderer.setPixelRatio(pixelRatio);
 
-    this.gridHelper = new GridHelper(1000, 100);
+    this.gridHelper = new THREE.GridHelper(1000, 100);
     this.scene.add(this.gridHelper);
 
-    this.box = new Mesh(
-      new BoxGeometry(60, 60, 60),
-      new MeshBasicMaterial({ color: 0xf5f5f5, })
+    this.box = new THREE.Mesh(
+      new THREE.BoxGeometry(60, 60, 60),
+      new THREE.MeshBasicMaterial({ color: 0xf5f5f5, })
     );
     this.box.position.z = -300;
     this.box.position.y = 30;
